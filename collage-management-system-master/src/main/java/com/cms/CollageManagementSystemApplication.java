b1b83871bc9f265f731e69a6ffba8c5ea60041c1
Rohit Ranjan <Rohit Ranjan@LAPTOP-V8PDAMNS>
2021-06-26 18:46:46 +0530

    fix error

---
+++ collage-management-system-master/src/main/java/com/cms/CollageManagementSystemApplication.java
@@ -0,0 +1,18 @@
+package com.cms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
+
+@SpringBootApplication
+public class CollageManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CollageManagementSystemApplication.class, args);
+		
+	}
+	
+	
+	}
+
